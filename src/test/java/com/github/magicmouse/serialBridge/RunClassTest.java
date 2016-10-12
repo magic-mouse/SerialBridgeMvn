a59d8cc399188514344abf39dc43e3dd3a9db82f
mail@magic-mouse.-net <mail@magic-mouse.-net>
2016-10-12 16:50:26 +0200

    travis file

---
+++ src/test/java/com/github/magicmouse/serialBridge/RunClassTest.java
@@ -0,0 +1,18 @@
+package com.github.magicmouse.serialBridge;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Peter on 2016-10-12.
+ */
+public class RunClassTest {
+
+
+    @Test
+    public static void testSerial(){
+
+    }
+
+}
